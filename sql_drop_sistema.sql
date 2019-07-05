bcd504d18715808ca3956329bede641d06335e19
Ewerton dougllas <42045809+ewerton255@users.noreply.github.com>
2019-07-05 14:27:44 -0300

    sql para apagar o banco de dados

---
+++ sql_drop_sistema.sql
@@ -0,0 +1,8 @@
+DROP TABLE rastreioencomendas.historico_pacote;
+DROP TABLE rastreioencomendas.pacote;
+DROP TABLE rastreioencomendas.frete;
+DROP TABLE rastreioencomendas.usuario;
+DROP TABLE rastreioencomendas.empresa;
+DROP TABLE rastreioencomendas.endereco;
+DROP TABLE rastreioencomendas.status;
+DROP SCHEMA rastreioencomendas;
